c7f335cb1b313e07f517ead3b07d80ae9545b17e
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-10-06 21:27:47 +0300

    EST-2: Реализован конструктор

--- src/Establishment.java
+++ src/Establishment.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Establishment {
+    private String name;
+    private String address;
+    private String category;
+    private String password;
+    private double averageBill;
+    private List<MenuItem> menu;
+    private List<Promotion> promotions;
+
+    public Establishment() {
+        this.menu = new ArrayList<>();
+        this.promotions = new ArrayList<>();
+    }
+
+    public Establishment(String name, String address, String category, String password, double averageBill) {
+        this.name = name;
+        this.address = address;
+        this.category = category;
+        this.password = password;
+        this.averageBill = 0.0;
+    }
+
+
 }
